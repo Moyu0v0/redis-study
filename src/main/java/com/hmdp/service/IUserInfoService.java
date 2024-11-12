35f4fb96141d2b9489ab5df84eb3ea7483c81fc6
WANG YUQING <yuqingwang761@gmail.com>
2024-11-12 15:34:35 +0800

    feat(短信登录): 新增发送短信验证码功能

--- src/main/java/com/hmdp/service/IUserInfoService.java
+++ src/main/java/com/hmdp/service/IUserInfoService.java
@@ -1,7 +1,10 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.UserInfo;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -12,5 +15,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2021-12-24
  */
 public interface IUserInfoService extends IService<UserInfo> {
-
+    /**
+     * 发送验证码
+     *
+     * @param phone   手机号码
+     * @param session 会话
+     * @return {@link Result }
+     */
+    Result sendCode(String phone, HttpSession session);
 }
