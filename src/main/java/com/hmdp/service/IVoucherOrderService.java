6fe662d3dbe3b32baef72b4f3ca7f60fe1616139
WANG YUQING <yuqingwang761@gmail.com>
2024-11-15 14:22:49 +0800

    feat(优惠券秒杀): 新增秒杀下单功能

--- src/main/java/com/hmdp/service/IVoucherOrderService.java
+++ src/main/java/com/hmdp/service/IVoucherOrderService.java
@@ -1,7 +1,8 @@
 package com.hmdp.service;
 
-import com.hmdp.entity.VoucherOrder;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.VoucherOrder;
 
 /**
  * <p>
@@ -12,5 +13,11 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2021-12-22
  */
 public interface IVoucherOrderService extends IService<VoucherOrder> {
-
+    /**
+     * 秒杀下单优惠券
+     *
+     * @param voucherId 券id
+     * @return {@link Result }
+     */
+    Result seckillVoucher(Long voucherId);
 }
--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,21 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+
+import static com.hmdp.utils.RedisConstants.VOUCHER_ORDER_KEY;
 
 /**
  * <p>
@@ -16,5 +27,49 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
+    @Resource
+    private ISeckillVoucherService iSeckillVoucherService;
 
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+        // 1. 查询秒杀优惠券信息
+        SeckillVoucher voucher = iSeckillVoucherService.getById(voucherId);
+        // 2. 判断秒杀是否开始
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            return Result.fail("秒杀尚未开始！");
+        }
+        // 3. 判断秒杀是否已经结束
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            return Result.fail("秒杀已经结束！");
+        }
+        // 4. 判断库存是否充足
+        if (voucher.getStock() < 1) {
+            return Result.fail("库存不足！");
+        }
+        // 5. 扣减库存
+        boolean success = iSeckillVoucherService.update()
+                .setSql("stock = stock - 1")
+                .eq("voucher_id", voucherId)
+                .update();
+        if (!success) {
+            return Result.fail("库存不足！");
+        }
+        // 6. 创建订单
+        VoucherOrder voucherOrder = new VoucherOrder();
+        // 6.1 订单id
+        Long orderId = redisIdWorker.nextId(VOUCHER_ORDER_KEY);
+        voucherOrder.setId(orderId);
+        // 6.2 用户id
+        voucherOrder.setUserId(UserHolder.getUser().getId());
+        // 6.3 代金券id
+        voucherOrder.setVoucherId(voucherId);
+
+        save(voucherOrder);
+        // 7. 返回订单id
+        return Result.ok(orderId);
+    }
 }
